89a904e41b49b949a47c2e10bf48d6dcd26ef2f9
sean-murray <sean.murray2000@hotmail.com>
2020-10-03 19:52:16 -0400

    isChainValid function to check for valid subsequent blocks in a chain.

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -4,7 +4,27 @@ import com.google.gson.GsonBuilder;
 public class BlockChain {
 
 
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
 
+        // Looping through blockchain to check hashes
+        for(int i = 1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i - 1);
+            // Comparing registered hash and calculated hash
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            // Comparing previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
