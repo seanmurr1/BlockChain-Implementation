06e94e187242194ab53a0d4679d3d2c258fe40be
sean-murray <sean.murray2000@hotmail.com>
2020-10-03 19:55:03 -0400

    mineBlock function for Block class.

--- src/Block.java
+++ src/Block.java
@@ -6,6 +6,7 @@ public class Block {
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -14,8 +15,20 @@ public class Block {
         this.hash = calculateHash();
     }
 
+    // Calculate has based on contents of block
     public String calculateHash() {
         String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
         return calculatedHash;
     }
+
+
+    public void mineBlock(int difficulty) {
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
 }
